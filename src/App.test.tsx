38a96e16e2e3d83db2945c2d654087da6c23e3f8
matbanasik <mateuszbanasik22@gmail.com>
2021-02-21 17:56:49 +0100

    add basic tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,43 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import { render, waitFor, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import App from './App';
+import users from './mocks/users.json';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const server = setupServer(
+    rest.get('https://jsonplaceholder.typicode.com/users', (req: any, res: any, ctx: any) => {
+        return res(ctx.json(users))
+    })
+)
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+test('It fetches users and display them in a list', async () => {
+    render(<App />);
+
+    const user = await waitFor(() => screen.getByText('Leanne Graham'));
+
+    expect(user).toBeInTheDocument();
+});
+
+test('It allows to fiter users by name', async () => {
+    render(<App />);
+
+    await waitFor(() => {
+        expect(screen.getByText('Leanne Graham')).toBeInTheDocument();
+        expect(screen.getByText('Ervin Howell')).toBeInTheDocument();
+    })
+
+    const input = screen.getByPlaceholderText(/search by user name.../i);
+    fireEvent.change(input, { target: { value: 'leanne' } });
+
+    await waitFor(() => {
+        expect(screen.getByText('Leanne Graham')).toBeInTheDocument();
+        expect(screen.queryByText('Ervin Howell')).not.toBeInTheDocument();
+    })
+    
 });
